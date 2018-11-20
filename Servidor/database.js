443b1ede3f9cd688e0fe7b37f8a28a2bbf19958b
kevin <kevingutierrezudea1996@gmail.com>
2018-11-20 09:12:44 -0500

    first commit

--- Servidor/database.js
+++ Servidor/database.js
@@ -1,1 +1,12 @@
 // es la encargada de la conexion con la base de datos
+
+const mongoose = require('mongoose');
+
+const URI = 'mongodb://localhost/cine'; // direccion de la base de datos 
+
+mongoose.connect(URI)
+    .then(db => console.log('is connected')) //muestra en consola si esta conectado
+    .catch(error => console.error(error)); //muestra en consola si esta desconectado
+
+
+module.exports = mongoose;
--- Servidor/index.js
+++ Servidor/index.js
@@ -1,8 +1,9 @@
 //confgurar el servidor cuando este empieza (encargado de arrancar el servidor)
 
 const express = require('express');
+const morgan = require('morgan'); //ayuda a ver por consola lo que el usuario esta pidiendo
 const app = express(); //app es la encargada de tener toda la funcionalidad del servidor
-
+const { mongoose } = require('./database'); // obtengo la conexion a la base de datos
 // dividiendo el servidor en multiples tareas
 
 // configuracion
@@ -12,6 +13,8 @@ app.set('port', process.env.PORT || 3000); //me indica que si en la nube me asig
 
 //funciones que procesa datos
 
+app.use(morgan('dev')); // siempre que exista una peticion pasara por aca
+app.use(express.json()); //ayuda al servidor a entender los datos que vienen desde el navegador en formato json
 //rutas del servdor 
 
 //iniciando el servidor
