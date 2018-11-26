083184c8e9f167da8e3367b3278a3fc6e71b7b45
kevin <kevingutierrezudea1996@gmail.com>
2018-11-26 16:05:46 -0500

    fisrt commit

--- frontend/src/app/models/reservas.ts
+++ frontend/src/app/models/reservas.ts
@@ -1,2 +1,12 @@
+// voy a definir la estructura con la que el front me reconoce lo que viene del back
 export class Reservas {
+    _id: String;
+    nombrePelicula: String;
+    nombrePersona: String;
+    nombreTeatro:  String;
+    horaReserva:  String;
+    ubicacion:  String;
+    fecha:  String;
+    precio: Number;
+
 }
--- frontend/src/app/services/reservas.service.ts
+++ frontend/src/app/services/reservas.service.ts
@@ -1,12 +1,20 @@
 //Aca tendre todos los metodos responsables de hacer las reservas, eliminarlas, modificarlas etc
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'; // importacion encargada de la conexion con mi servidor
+import { Reservas } from '../models/reservas';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReservasService {
 
+ selecionarReserva: Reservas;
+
   //la propiedad HttpClient me permite hacer consultas al servidor
   constructor( private http: HttpClient) { }
+
+  getReservas(){
+    //http me deja hacer las consultas...al servidor
+    return this.http.get('http://localhost:3000/api/reservas');
+  }
 }
